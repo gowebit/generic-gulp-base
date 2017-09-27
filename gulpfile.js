866fee492e104e109d0874f6c7b70b040e4c6296
Gabriel Gomes <gabriel_gom@hotmail.com>
2017-09-27 15:31:24 -0300

    Fix: Relative URLs inside CSS files were breaking with gulp-clean-css minification

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,9 @@ gulp.task('minify-css', function() {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(cleanCSS())
+        .pipe(cleanCSS({ 
+            rebase: false
+        }))
         .pipe(gulp.dest('dist'));
 });
 
